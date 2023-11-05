f0115e3ea63ca4de141cb9c41cf90961b06a249a
CamilaNavM <144072023+CamilaNavM@users.noreply.github.com>
2023-11-05 01:28:15 -0600

    Scripts
    se hizo un folder para contener scripts

--- CRCamila/Assets/scripts/FileSystem.cs
+++ CRCamila/Assets/scripts/FileSystem.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-public class Fil : MonoBehaviour
+public class FileSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject obj;
+    public bool isSavingPosition = false;
+    v
+    
+
+    void SaveObjectPosition(Transform _objTransform)
+
     {
-        
+        // 1) Acceder al path del archivo 
+        string filename = "positions";
+        string extension = ".txt";
+        string path = Application.dataPath + "/Resources/" +
+          filename + extension;
+        // 2) Craer un archivo si no existe otro con el mismo nombre
+        if (File.Exists(path))
+        {
+            // 3) Escribir dentro del archivo 
+
+            File.WriteAllText(path, "Hola");
+        }
+        // 4) Almacenar el contenido del archivo 
+        string data = "Position: " + "(" + _objTransform.position.x.ToString() + ","
+            + _objTransform.position.y.ToString() + ","
+            + _objTransform.position.z.ToString() + ","
+            + "/n";
+
+        // 5) Agregar la informacion del archivo
+        File.AppendAllText(path, data);
     }
 
-    // Update is called once per frame
-    void Update()
+   // private void Start()
+    /// <summary>
+    /// {
+    /// </summary>
+      //  CreateFile();
+    // }
+
+    private void Update()
     {
+        if (isSavingPosition) 
+        {
+            SaveObjectPosition(obj.transform);
+        }
         
     }
 }
